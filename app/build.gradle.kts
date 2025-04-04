25de4168e3e92e0455ce2859f36457beedd0dbd5
Swapnil Jadhav <swapnil.jadhav@kohler.com>
2025-04-04 18:56:49 +0530

    Update - version code auto increment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,11 +12,11 @@ android {
         applicationId = "com.sj.unittestworkshop"
         minSdk = 31
         targetSdk = 35
-        //versionCode = 1
-        //versionName = "1.0"
-        versionCode = project.properties["versionCode"].toString().toInt()
+        versionCode = 1
+        versionName = "1.0"
+        /*versionCode = project.properties["versionCode"].toString().toInt()
         versionName = project.properties["versionName"].toString()
-
+*/
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
