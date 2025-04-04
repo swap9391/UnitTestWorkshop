e7dd5508726c08d3001068333e24d3281d284756
Swapnil Jadhav <swapnil.jadhav@kohler.com>
2025-04-04 20:59:28 +0530

    Update - version code auto increment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+println("versionCode from properties: ${project.properties["versionCode"]}")
+println("versionName from properties: ${project.properties["versionName"]}")
+
 android {
     namespace = "com.sj.unittestworkshop"
     compileSdk = 35
